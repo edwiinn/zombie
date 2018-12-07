b3a9cf693736c15c765ea03f178bd7b70ae81c9c
lp <tirtajiwanggataufiq@gmail.com>
2018-12-07 22:14:58 +0700

    belajar load model ke js

--- dev/js/main.js
+++ dev/js/main.js
@@ -2,29 +2,75 @@
 
 var gun;
 
-(function(){    // Auto run function
-    // declaration of important variables
-    container_width = document.getElementById("container").width;
-    container_height = document.getElementById("container").height;
-    loader = new THREE.JSONLoader();  // used to load JSON file of model
-    loader.load( 'assets/.json', function ( geometry, materials ) {
-        gun = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial( materials ) );
-        scene.add( gun );
-    });
-    renderer = new THREE.WebGLRenderer({antialias: true});
-    scene = new THREE.Scene();
-    camera = new THREE.OrthographicCamera( container_width / - 2, container_width / 2, container_height / 2, container_height / - 2, 1, 1000 );
-    light = new THREE.AmbientLight(0x404040);
-    clock = new THREE.Clock();
-    scene.add(camera, light);
-
-})();
 
 function animate(){
+    camera.updateProjectionMatrix();
     render();
+    requestAnimationFrame( animate );
 }
 
 function render(){
-    
     renderer.render(scene, camera);
+}
+
+(function(){    // Auto run function
+    // declaration of important variables
+    loader = new THREE.ObjectLoader();  // used to load JSON file of model
+    loader.setCrossOrigin("use-credentials");
+    loadObject("gun.json", "gun");
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0xffffff, 1);
+    container.appendChild(renderer.domElement);
+    scene = new THREE.Scene();
+    // camera = new THREE.OrthographicCamera( window.innerWidth / - 2, container_width / 2, window.innerHeight / 2, window.innerHeight / - 2, 1, 1000 );
+    camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.2, 25000);
+    camera.position.set(20, 10, 20);
+    camera.lookAt(0,0,0);
+    light = new THREE.AmbientLight(0x404040);
+    clock = new THREE.Clock();
+    scene.add(camera, light);
+    // debug corner
+
+    // end
+    animate();
+
+})();
+
+
+function loadObject(filename, objName){
+    loader.load(
+        // resource URL
+        "assets/"+filename,
+    
+        // onLoad callback
+        // Here the loaded data is assumed to be an object
+        function ( obj ) {
+            // Add the loaded object to the scene
+            obj.name = objName;
+            scene.add( obj );
+        },
+
+        // onProgress callback
+        function ( xhr ) {
+            console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+        },
+    
+        // onError callback
+        function ( err ) {
+            console.error( 'An error happened' );
+        }
+    );
+}
+
+function getObjectFromScene(objName){
+    scene.traverse (function (object)
+    {
+        
+        if (object.name == objName){
+            console.log("found");
+          
+            return object;
+        }
+    });
 }
