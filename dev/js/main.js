a61ce90207a254279fe7ac97d2ab84e6aa82a78a
lp <tirtajiwanggataufiq@gmail.com>
2018-12-07 20:46:50 +0700

    add gun model json file

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,11 +1,22 @@
 "use strict";
 
+var gun;
+
 (function(){    // Auto run function
     // declaration of important variables
+    container_width = document.getElementById("container").width;
+    container_height = document.getElementById("container").height;
+    loader = new THREE.JSONLoader();  // used to load JSON file of model
+    loader.load( 'assets/.json', function ( geometry, materials ) {
+        var mesh = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial( materials ) );
+        scene.add( mesh );
+    });
     renderer = new THREE.WebGLRenderer({antialias: true});
     scene = new THREE.Scene();
-    light = new THREE.AmbientLight();
+    camera = new THREE.OrthographicCamera( container_width / - 2, container_width / 2, container_height / 2, container_height / - 2, 1, 1000 );
+    light = new THREE.AmbientLight(0x404040);
     clock = new THREE.Clock();
+    scene.add(camera, light);
 
 })();
 
@@ -14,5 +25,6 @@ function animate(){
 }
 
 function render(){
+    
     renderer.render(scene, camera);
 }
