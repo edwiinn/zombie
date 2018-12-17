a8ab1f48944c128bb5a74d1222fcee100499ab53
Rahma Sofyantoro <rahma.sofyantoro16@mhs.if.its.ac.id>
2018-12-17 12:26:18 +0700

    perbaikan gltf loader

--- dev/js/ourlib.js
+++ dev/js/ourlib.js
@@ -2,8 +2,10 @@ function animate(){
     deltaTime = clock.getDelta();
     requestAnimationFrame( animate );
     camera.updateProjectionMatrix();
-    if ( mixer ) mixer.update( deltaTime );
-    render();
+
+      for(var i=0;i<mixer.length;i++)
+      if ( mixer[i] ) mixer[i].update( deltaTime );
+      render();
 }
 
 function render(){
@@ -16,7 +18,7 @@ function LockCamera(){
 }
 
 
-function loadObject(filename, parentName, objName){
+function loadObject (filename, parentName, objName){
     objectLoader.load(
         "assets/"+filename,
         function ( obj ) {
@@ -36,24 +38,29 @@ function loadObject(filename, parentName, objName){
     );
 }
 
-function loadObjectGLTF(filename, parentName, objName,scale,generateAnimation){ //scale = [[1],[2],[3]]
-    console.log(filename);
-    loaderGLTF_m = new THREE.GLTFLoader();
-    loaderGLTF_m.load(
+function loadObjectGLTF(filename, parentName, objName,scale,position,generateAnimation){
+    loaderGLTF_m = new THREE.GLTFLoader();                  //position =[[x],[y],[z]]
+    loaderGLTF_m.load(                                     //scale =[[x],[y],[z]]
         "assets/"+filename,
         function ( obj ) {
-            // Add the loaded object to the scene
-          //  scane.add(gltf.scene);
              if(scene.getObjectByName(objName) == null){
                  var tmp = new THREE.Group();
                  tmp.name = parentName;
                  obj.scene.name = objName;
+                 if(scale!=null){
                  obj.scene.scale.x=scale[0];
                  obj.scene.scale.y=scale[1];
                  obj.scene.scale.z=scale[2];
+                 }
+                 if(position!=null){
+                 obj.scene.position.x=position[0];
+                 obj.scene.position.y=position[1];
+                 obj.scene.position.z=position[2];
+                 }
                  tmp.add(obj.scene);
                  scene.add(tmp );
                  modDir[parentName]=obj.scene;
+                 if(generateAnimation!=null)
                  generateAnimation(modDir[parentName],obj.animations);
                  console.log(objName+" have been loaded");
                  return;
@@ -66,4 +73,12 @@ function loadObjectGLTF(filename, parentName, objName,scale,generateAnimation){ 
 }
 
 
-const delay = (duration) => new Promise(resolve => setTimeout(resolve, duration))
+
+//tambahan
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+//const delay = (duration) => new Promise(resolve => setTimeout(resolve, duration))
