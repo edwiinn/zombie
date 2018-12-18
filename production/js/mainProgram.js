a4d13a8404af4b974de7c8ec70cc51b81a87251e
Rahma Sofyantoro <rahma.sofyantoro16@mhs.if.its.ac.id>
2018-12-19 02:56:45 +0700

    add score hp

--- production/js/mainProgram.js
+++ production/js/mainProgram.js
@@ -3,6 +3,8 @@
 async function main() {
     initGame();
     await delay(5000);
+    document.getElementById('skorboard').style.display = "block";
+    document.getElementById('hpboard').style.display = "block";
     document.getElementById('image-loading').style.display = "none";
     // game start
     animate();
